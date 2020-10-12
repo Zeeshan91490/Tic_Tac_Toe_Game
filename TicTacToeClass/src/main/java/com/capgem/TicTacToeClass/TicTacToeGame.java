70d9f50d53cadf8747ec880159999144f0a3f8d7
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-13 00:20:22 +0530

    TicTacToeClass/

--- TicTacToeClass/src/main/java/com/capgem/TicTacToeClass/TicTacToeGame.java
+++ TicTacToeClass/src/main/java/com/capgem/TicTacToeClass/TicTacToeGame.java
@@ -1,8 +1,34 @@
-package employeewageproblem;
+package com.capgemini;
 
-public class employeewageproblem {
-	public static void main(String[] args){
-	      System.out.println("Welcome to TicTacToeGame");
+import java.util.Scanner;
+
+public class TicTacToeGame {
+	private static final char EMPTY = ' ';
+	private static final char CHARACTER_X = 'X';
+	private static final char CHARACTER_O = 'O';
 	
+	/**
+	 * uc5
+	 * @param ticTacToeBoard
+	 * @param playerLetter
+	 * @param moveIndex
+	 */
+	private static void makeMove(char[] ticTacToeBoard, char playerLetter, int moveIndex) {
+		ticTacToeBoard[moveIndex] = playerLetter;
 	}
+
+	public static void main (String[] args) {
+		Scanner takeInput = new Scanner(System.in);
+		char[] ticTacToeBoard = createBoard();
+		System.out.println("Player letter (X or O): ");
+		char playerLetter = takeInput.next().charAt(0);
+		char computerLetter = selectLetter(playerLetter);
+		System.out.println("Computer Letter: " + computerLetter);
+		showBoard(ticTacToeBoard);
+		int moveIndex = checkFree(ticTacToeBoard, playerLetter);
+		makeMove(ticTacToeBoard, playerLetter, moveIndex);
+		showBoard(ticTacToeBoard);
+	}	
 }
+
+
