8a10696e4f1393265f2c2b6d35c8b05a94d77b40
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-13 00:16:45 +0530

    TicTacToeClass/

--- TicTacToeClass/src/main/java/com/capgem/TicTacToeClass/TicTacToeGame.java
+++ TicTacToeClass/src/main/java/com/capgem/TicTacToeClass/TicTacToeGame.java
@@ -1,8 +1,19 @@
-package employeewageproblem;
+package com.capgemini;
 
-public class employeewageproblem {
-	public static void main(String[] args){
-	      System.out.println("Welcome to TicTacToeGame");
-	
+import java.util.Scanner;
+
+public class TicTacToeGame {
+	private static final char EMPTY = ' ';
+	private static final char CHARACTER_X = 'X';
+	private static final char CHARACTER_O = 'O';
+	private static void showBoard(char[] ticTacToeBoard) {
+		for(int rowHead = 1; rowHead <= 7; rowHead += 3) {
+			for(int cellHead = rowHead; cellHead < rowHead + 3; cellHead++) {
+				System.out.print(ticTacToeBoard[cellHead]);
+				if(cellHead % 3 != 0)
+					System.out.print(" | ");
+			}
+			System.out.print("\n");
+		}
 	}
-}
+
